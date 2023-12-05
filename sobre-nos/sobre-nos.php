975071b2b887432b4f610eab322229af2d5b64f0
vodreck <120072053+vodreck@users.noreply.github.com>
2023-12-04 21:09:52 -0300

    Add files via upload

---
+++ sobre-nos/sobre-nos.php
@@ -0,0 +1,135 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/Header/header.php';
+    ?> 
+  
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Home/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>
+        Sobre Nós
+    </title>
+</head>
+<body>
+
+
+    <div  id="container1" class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Sobre Nós</h2>
+                <p>Bem-vindo ao coração do nosso universo de café, onde cada xícara é uma história em si mesma.
+                     Na Cafeteria Gourmet, somos apaixonados por mais do que apenas café, 
+                     somos apaixonados por experiências que despertam os sentidos e criam memórias duradouras.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="/sobre-nos/img/logo.png" alt="Nossa Loja" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+    <div  id="container1" class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Nossa História</h2>
+                <p>Nossa jornada começou há muitos anos, quando nosso fundador, um amante de café ávido, embarcou em uma busca para descobrir os segredos por trás do café perfeito.
+                     Viajando pelos cantos mais remotos do mundo em busca dos melhores grãos, ele mergulhou nas tradições culturais que cercam o café.
+                     Essa busca incessante por qualidade e autenticidade resultou na criação da Cafeteria Gourmet.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="/sobre-nos/img/fruta-cafe.png" alt="Nossa Loja" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+    <div  id="container1" class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Nossa Missão</h2>
+                <p>Na Pete's Coffee, nossa missão é oferecer mais do que apenas café. Buscamos proporcionar uma experiência completa para todos os amantes da bebida dos deuses.
+                     Acreditamos que o café é uma arte que deve ser apreciada e compartilhada. Dedicamos nosso tempo e energia para:
+                     <br>
+                     <br>
+◈Selecionar os Melhores Grãos: Viajamos ao redor do mundo para encontrar os grãos mais excepcionais, garantindo a qualidade desde o início.
+<br>
+<br>
+◈Arte na Preparação: Nossos baristas são verdadeiros artistas, dominando a arte da preparação e apresentação do café perfeito.
+<br>
+<br>
+◈Ambiente Aconchegante: Criamos um espaço acolhedor e relaxante onde você pode saborear sua xícara de café com tranquilidade.
+<br>
+<br>
+◈Experimentação: Oferecemos uma variedade de blends e métodos de preparo para que você possa explorar novos sabores e aromas.
+<br>
+<br>
+◈Sustentabilidade: Comprometemo-nos a apoiar práticas sustentáveis, tanto na seleção de grãos quanto nas operações diárias da cafeteria.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="/sobre-nos/img/missao.png" alt="Nossa Loja" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+    <div  id="container1" class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Venha nos Visitar</h2>
+                <p>Convidamos você a se juntar a nós nesta jornada de descoberta e prazer.
+                     Explore nossa seleção de cafés, desfrute de nossos deliciosos acompanhamentos e mergulhe na atmosfera única que só a Cafeteria Gourmet pode oferecer.
+<br>
+<br>
+Nosso compromisso é criar momentos especiais para você, seus amigos e sua família, sempre com uma xícara de café excepcional nas mãos.
+<br>
+<br>
+Sinta-se em casa na Cafeteria Gourmet, onde o café é mais do que uma bebida - é uma paixão que compartilhamos com você.
+<br>
+<br>
+<b>
+Horário de Funcionamento:
+</b>
+<br>
+<br>
+Segunda a Sexta: 7h00 - 20h00
+<br>
+Sábado e Domingo: 8h00 - 18h00
+<br>
+<br>
+Esperamos vê-lo em breve!</p>
+            </div>
+            <div class="col-md-6">
+                <img src="/sobre-nos/img/fachada.png" alt="Nossa Loja" class="img-fluid">
+                <p>R. Cruzeiro, 830 - Barra Funda, São Paulo - SP, 01137-000</p>
+            </div>
+        </div>
+    </div>
+
+    <div  id="container1" class="container mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Pronto Para Entrega</h2>
+                <p>Bem-vindo à nossa loja online de produtos gourmet! Aqui oferecemos a você nosso delivery com todo nosso cardápio, venha conhecer nossos produtos</p>
+                <button class="botao1" onclick="window.location.href='/Pagina de Produtos/pagina_produtos.php'" type="button">Produtos</button>
+            </div>
+            
+            <div class="col-md-6">
+                <img src="/Home/img/Propaganda.png" alt="Nossa Loja" class="img-fluid">
+            </div>
+            
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+<?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/Footer/footer.php';
+    ?> 
